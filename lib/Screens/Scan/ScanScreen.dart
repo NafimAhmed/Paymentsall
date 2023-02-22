d0efae8719add0438f45dbc80e8706c0b018aec7
recen <recentnafimahmed@gmail.com>
2023-02-22 12:30:35 +0600

    nafim QR code created added 10:57 22/2/2023

--- lib/Screens/Scan/ScanScreen.dart
+++ lib/Screens/Scan/ScanScreen.dart
@@ -1,6 +1,9 @@
 import 'package:barcode_widget/barcode_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../utils/app_layout.dart';
 
 class ScanScreen extends StatefulWidget {
   const ScanScreen({Key? key}) : super(key: key);
@@ -21,8 +24,15 @@ class _ScanScreenState extends State<ScanScreen> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
 
-
-
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+                child: Text("Your QR code is :",
+                style: GoogleFonts.openSans(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold
+                ),
+                )
+            ),
 
 
             Center(
@@ -45,7 +55,84 @@ class _ScanScreenState extends State<ScanScreen> {
                   ),
                 ],
               ),
-            )
+            ),
+
+
+
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(20),vertical: AppLayout.getHeight(20)),
+              child: ElevatedButton(
+
+                onPressed: () {
+
+                  // Navigator.pop(context);
+                  //
+                  // // Navigator.push(
+                  // //     context,
+                  // //     MaterialPageRoute(
+                  // //         builder: (context) =>
+                  // //             BottomBar()));
+                  //
+                  // Get.to(BottomBar(),
+                  //     duration: Duration(milliseconds: 500), //duration of transitions, default 1 sec
+                  //     transition: Transition.rightToLeft );
+
+
+
+
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: 60.0, vertical: 15.0),
+                  primary: Colors.red.shade900,
+                  shape: StadiumBorder(),
+                ),
+                child: Text(
+                  "Download QR Code",
+                  style: TextStyle(color: Colors.white, fontSize: 18,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+            ),
+
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(20),vertical: AppLayout.getHeight(20)),
+              child: ElevatedButton(
+
+                onPressed: () {
+
+                  // Navigator.pop(context);
+                  //
+                  // // Navigator.push(
+                  // //     context,
+                  // //     MaterialPageRoute(
+                  // //         builder: (context) =>
+                  // //             BottomBar()));
+                  //
+                  // Get.to(BottomBar(),
+                  //     duration: Duration(milliseconds: 500), //duration of transitions, default 1 sec
+                  //     transition: Transition.rightToLeft );
+
+
+
+
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: 60.0, vertical: 15.0),
+                  primary: Colors.red.shade900,
+                  shape: StadiumBorder(),
+                ),
+
+                child: Text(
+                  "Share QR Code",
+                  style: TextStyle(color: Colors.white, fontSize: 18,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+            ),
 
 
 
