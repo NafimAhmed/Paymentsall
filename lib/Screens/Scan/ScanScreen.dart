c43fb76ce82742e1b85c8fc8c31a80ae0437875a
recen <recentnafimahmed@gmail.com>
2023-02-22 10:58:01 +0600

    nafim more dependency added 10:57 22/2/2023

--- lib/Screens/Scan/ScanScreen.dart
+++ lib/Screens/Scan/ScanScreen.dart
@@ -1,3 +1,4 @@
+import 'package:barcode_widget/barcode_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -13,6 +14,46 @@ class _ScanScreenState extends State<ScanScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFFFF8F8),
+
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+
+
+
+
+            Center(
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  BarcodeWidget(
+                    barcode: Barcode.qrCode(
+                      errorCorrectLevel: BarcodeQRCorrectionLevel.high,
+                    ),
+                    data: '01797609439',
+                    width: 200,
+                    height: 200,
+                  ),
+                  Container(
+                    color: Colors.white,
+                    width: 60,
+                    height: 60,
+                    child: const FlutterLogo(),
+                  ),
+                ],
+              ),
+            )
+
+
+
+
+
+          ],
+        ),
+      ),
 
 
     );
