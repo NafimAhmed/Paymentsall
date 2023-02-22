43397f4385c742a40cbc80071cf784c132ac5d22
recen <recentnafimahmed@gmail.com>
2023-02-22 18:03:19 +0600

    nafim Transaction list created  Updated 6:03pm 22/02/2023

--- lib/Screens/RecentTransfer/RecenTransferScreen.dart
+++ lib/Screens/RecentTransfer/RecenTransferScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:payments_all_app/utils/app_layout.dart';
 
 class RecentTransferScreen extends StatefulWidget {
   const RecentTransferScreen({Key? key}) : super(key: key);
@@ -13,22 +14,59 @@ class _RecentTransferScreenState extends State<RecentTransferScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFFFF8F8),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            SizedBox(height: 30,),
-            Row(
-              children: [
-                TextButton(
-                    onPressed: (){},
-                    child: Icon(Icons.arrow_back_ios,color: Colors.black,)),
-                Text('All Transfer',style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold,),)
-              ],
-            ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(height: 30,),
+              Row(
+                children: [
+                  TextButton(
+                      onPressed: (){},
+                      child: Icon(Icons.arrow_back_ios,color: Colors.black,)),
+                  Text('All Transections',style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold,),)
+                ],
+              ),
 
 
-          ],
+
+              ListView.builder(
+                shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: 10,
+                  itemBuilder: (BuildContext context,int index){
+
+                    return Container(
+                      margin: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(10),vertical: AppLayout.getHeight(20)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+
+                          Text("Pay Bill"),
+
+                          Text("DESCO"),
+
+                          Text("Transaction ID : DEX123546"),
+
+                          Text("12:50 pm, 22/02/2023"),
+
+
+
+                        ],
+                      ),
+                    );
+
+              }),
+
+
+
+
+
+
+
+            ],
+          ),
         ),
       ));
   }
