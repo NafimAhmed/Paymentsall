6f3fd91aa59fa61058d13a4c13ae2e02312c5394
recen <recentnafimahmed@gmail.com>
2023-03-05 18:49:44 +0600

    nafim Travel card delivary status repair 5:02pm 05/03/2023

--- lib/Screens/travelcard_all/card_status.dart
+++ lib/Screens/travelcard_all/card_status.dart
@@ -2,7 +2,6 @@
 
 
 
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -20,153 +19,153 @@ class CardStatus extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
 
 
 
 
-          Row(
-            children: [
-              TextButton(
-                  style: ButtonStyle(
-                    shadowColor: MaterialStateProperty.all(Colors.transparent),
-                    overlayColor: MaterialStateProperty.all(Colors.transparent),
-                  ),
-                  onPressed: (){
-                    Navigator.pop(context);
-                  },
-                  child: const Icon(Icons.arrow_back,color: Colors.black,)),
-              const Text('Delivary status',style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),),
-            ],
-          ),
-
-
-
-
-          Container(
-
-            width: AppLayout.getScreenWidth(),
-
-
-            padding: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(20),vertical: AppLayout.getHeight(20)),
-            decoration: BoxDecoration(
-                color: Colors.red.shade100,
-                borderRadius: BorderRadius.all(Radius.circular(15))
-            ),
-            margin: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(10),vertical: AppLayout.getHeight(20)),
-
-
-
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            Row(
               children: [
-
-                Text("Collecting point :",
-                  style: GoogleFonts.openSans(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold
-                  ),
-                ),
-
-
-                Text("Branch name",
-                  style: GoogleFonts.openSans(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold
-                  ),
-                ),
-                Text("Location")
-
+                TextButton(
+                    style: ButtonStyle(
+                      shadowColor: MaterialStateProperty.all(Colors.transparent),
+                      overlayColor: MaterialStateProperty.all(Colors.transparent),
+                    ),
+                    onPressed: (){
+                      Navigator.pop(context);
+                    },
+                    child: const Icon(Icons.arrow_back,color: Colors.black,)),
+                const Text('Delivary status',style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),),
               ],
             ),
-          ),
 
 
-          Container(
-
-            padding: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(05),vertical: AppLayout.getHeight(20)),
-            decoration: BoxDecoration(
-                color: Colors.red.shade100,
-                borderRadius: BorderRadius.all(Radius.circular(15))
-            ),
-            margin: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(10),vertical: AppLayout.getHeight(20)),
 
 
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
+            Container(
+
+              width: AppLayout.getScreenWidth(),
 
 
-                Text(" Card Status :",
-                style: GoogleFonts.openSans(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold
-                ),
-                ),
-                Container(
-                  height: 120,
-                  //alignment: Alignment.topCenter,
-                  child: Timeline.tileBuilder(
-                    shrinkWrap: true,
-                    padding: EdgeInsets.all(5),
-                    theme: TimelineThemeData(
-                      direction: Axis.horizontal,
-                      connectorTheme: ConnectorThemeData(space: 8.0, thickness: 2.0),
+              padding: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(20),vertical: AppLayout.getHeight(20)),
+              decoration: BoxDecoration(
+                  color: Colors.red.shade100,
+                  borderRadius: BorderRadius.all(Radius.circular(15))
+              ),
+              margin: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(10),vertical: AppLayout.getHeight(20)),
+
+
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+
+                  Text("Collecting point :",
+                    style: GoogleFonts.openSans(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold
                     ),
-                    builder: TimelineTileBuilder.connected(
-                      connectionDirection: ConnectionDirection.before,
-                      itemCount: 4,
-                      itemExtentBuilder: (_, __) {
-                        return (AppLayout.getScreenWidth() - 40) / 4.0;
-                      },
-                      oppositeContentsBuilder: (context, index) {
-                        return Container();
-                      },
-                      contentsBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.only(top: 15.0),
-                          child: Text(
-                            _processes[index],
-                            style: GoogleFonts.openSans(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w500
+                  ),
+
+
+                  Text("Branch name",
+                    style: GoogleFonts.openSans(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Text("Location")
+
+                ],
+              ),
+            ),
+
+
+            Container(
+
+              padding: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(05),vertical: AppLayout.getHeight(20)),
+              decoration: BoxDecoration(
+                  color: Colors.red.shade100,
+                  borderRadius: BorderRadius.all(Radius.circular(15))
+              ),
+              margin: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(10),vertical: AppLayout.getHeight(20)),
+
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+
+
+                  Text(" Card Status :",
+                  style: GoogleFonts.openSans(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold
+                  ),
+                  ),
+                  Container(
+                    height: 120,
+                    //alignment: Alignment.topCenter,
+                    child: Timeline.tileBuilder(
+                      shrinkWrap: true,
+                      padding: EdgeInsets.all(5),
+                      theme: TimelineThemeData(
+                        direction: Axis.horizontal,
+                        connectorTheme: ConnectorThemeData(space: 8.0, thickness: 2.0),
+                      ),
+                      builder: TimelineTileBuilder.connected(
+                        connectionDirection: ConnectionDirection.before,
+                        itemCount: _processes.length,
+                        itemExtentBuilder: (_, __) {
+                          return (AppLayout.getScreenWidth() - 40) / 4.0;
+                        },
+                        oppositeContentsBuilder: (context, index) {
+                          return Container();
+                        },
+                        contentsBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.only(top: 15.0),
+                            child: Text(
+                              _processes[index],
+                              style: GoogleFonts.openSans(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500
+                              ),
                             ),
-                          ),
-                        );
-                      },
-                      indicatorBuilder: (_, index) {
+                          );
+                        },
+                        indicatorBuilder: (_, index) {
 
-                        if (index <= 1) {
-                          return DotIndicator(
-                            size: 20.0,
-                            color: Colors.green,
-                          );
-                        } else {
-                          return OutlinedDotIndicator(
-                            borderWidth: 4.0,
-                            color: Colors.green,
-                          );
-                        }
-                      },
-                      connectorBuilder: (_, index, type) {
-                        if (index > 0) {
-                          return SolidLineConnector(
-                            color: Colors.green,
-                          );
-                        } else {
-                          return null;
-                        }
-                      },
+                          if (index <= 1) {
+                            return DotIndicator(
+                              size: 20.0,
+                              color: Colors.green,
+                            );
+                          } else {
+                            return OutlinedDotIndicator(
+                              borderWidth: 4.0,
+                              color: Colors.green,
+                            );
+                          }
+                        },
+                        connectorBuilder: (_, index, type) {
+                          if (index > 0) {
+                            return SolidLineConnector(
+                              color: Colors.green,
+                            );
+                          } else {
+                            return null;
+                          }
+                        },
+                      ),
                     ),
-                  ),
-                )
+                  )
 
-              ],
-            ),
-          )
-
+                ],
+              ),
+            )
 
 
 
@@ -176,7 +175,9 @@ class CardStatus extends StatelessWidget{
 
 
 
-        ],
+
+          ],
+        ),
       ),
     );
   }
