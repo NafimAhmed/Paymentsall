7d797daaf8516025d3d0f8625f5b423da77636a4
recen <recentnafimahmed@gmail.com>
2023-03-06 14:51:04 +0600

    nafim Government fee confirmation done 5:02pm 05/03/2023

--- lib/Screens/Government Fees/ConfirmPaymentPage.dart
+++ lib/Screens/Government Fees/ConfirmPaymentPage.dart
@@ -1,7 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:gap/gap.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:payments_all_app/utils/app_layout.dart';
 
-class ConfirmPaymentPage extends StatelessWidget {
+class ConfirmPaymentPage extends StatefulWidget {
   const ConfirmPaymentPage({Key? key}) : super(key: key);
+
+  @override
+  State<ConfirmPaymentPage> createState() => _ConfirmPaymentPageState();
+}
+
+class _ConfirmPaymentPageState extends State<ConfirmPaymentPage> with TickerProviderStateMixin{
+
+
+  ////////////////////////////////////////////////////////
+
+  late AnimationController controller= AnimationController(
+    /// [AnimationController]s can be created with `vsync: this` because of
+    /// [TickerProviderStateMixin].
+    vsync: this,
+    duration: const Duration(seconds: 10),
+  )..addListener(() {
+
+    if(controller.value>=.90){
+
+      controller.stop();
+
+      Fluttertoast.showToast(
+          msg: "Done",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0
+      );
+
+    }
+    setState(() {
+
+
+
+
+
+    });
+  });
+
+  bool determinate = false;
+
+  @override
+  void dispose() {
+    controller.dispose();
+
+    Fluttertoast.showToast(
+        msg: "Done",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0
+    );
+
+
+    super.dispose();
+  }
+
+  //////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +86,186 @@ class ConfirmPaymentPage extends StatelessWidget {
         iconTheme: const IconThemeData(color: Colors.black),
         elevation: 0.0,
       ),
+
+
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal:AppLayout.getWidth(20)),
+        child: Column(
+          children: [
+
+
+
+            SizedBox(height: 40,),
+            Container(
+              height: 90,
+              width: 330,
+              child: ListTile(
+                leading: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: Image.asset('assets/images/ait.png',height: 30,)),
+                title: Text('Land Development(LD) Tax'),
+                subtitle: Text('Govt. Fees'),
+              ),
+            ),
+            SizedBox(height: 10,),
+
+
+            Table(
+              border: TableBorder.all(
+                  borderRadius: BorderRadius.circular(3.0),
+                  color: Colors.grey,
+                  style: BorderStyle.solid,
+                  width: 1),
+
+              children: [
+                TableRow(
+                    decoration: BoxDecoration(
+                        color: Colors.white
+                    ),
+                    children: [
+                      Column(children:[Text('Account Number \n 01771389547', style: GoogleFonts.openSans(fontSize: 16.0))]),
+                      Column(children:[Text('Account Name \nNafim Ahmed', style: GoogleFonts.openSans(fontSize: 16.0))]),
+                    ]
+                ),
+
+              ],
+            ),
+
+            Gap(20),
+
+            Table(
+              border: TableBorder.all(
+                  borderRadius: BorderRadius.circular(3.0),
+                  color: Colors.grey,
+                  style: BorderStyle.solid,
+                  width: 1),
+
+              children: [
+                TableRow(
+                    decoration: BoxDecoration(
+                        color: Colors.white
+                    ),
+                    children: [
+                      Column(children:[Text('Amount\n', style: GoogleFonts.openSans(fontSize: 16.0))]),
+                      Column(children:[Text('Charge\n', style: GoogleFonts.openSans(fontSize: 16.0))]),
+                      Column(children:[Text('Total\n', style: GoogleFonts.openSans(fontSize: 16.0))]),
+                    ]),
+                TableRow(
+                    decoration: BoxDecoration(
+                        color: Colors.white
+                    ),
+                    children: [
+                      Column(children:[Text('৳ ', style: TextStyle(fontSize: 16.0))]),
+                      Column(children:[Text('+৳ 0.00', style: TextStyle(fontSize: 16.0))]),
+                      Column(children:[Text('৳ ', style: TextStyle(fontSize: 16.0))]),
+                    ]),
+
+              ],
+            ),
+
+
+            ///////////////////////////////////////////////////
+
+
+
+            ////////////////////////////////
+
+            LinearProgressIndicator(
+              value: controller.value,
+              semanticsLabel: 'Linear progress indicator',
+
+            ),
+
+
+
+            ////////////////////////////
+
+
+
+
+
+
+
+
+
+            InkWell(
+              onTapDown: (Detail) async {
+                //startRecording();
+                //controller.reset();
+                controller.repeat();
+
+                if(controller.value>=.99){
+
+                  controller.stop();
+
+                }
+
+
+
+              },
+              child: Container(
+                padding: EdgeInsets.all(40),
+                margin: EdgeInsets.all(20),
+
+                decoration: BoxDecoration(
+                  color: Colors.red.shade900,
+                  // border: Border.all(width: 3.0),   // Set border width
+                  borderRadius: BorderRadius.all(
+                      Radius.circular(70.0)), // Set rounded corner radius
+                  //boxShadow: [BoxShadow(blurRadius: 10,color: Colors.black,offset: Offset(1,3))] // Make rounded corner of border
+                ),
+
+                child: Text("Tap to\n Confirm",
+                  style: GoogleFonts.openSans(
+                      fontSize: 25
+                  ),
+                ),
+              ),
+              onTapUp: (detail) {
+                controller.stop();
+
+
+                Fluttertoast.showToast(
+                    msg: controller.value.toString(),
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.CENTER,
+                    timeInSecForIosWeb: 1,
+                    backgroundColor: Colors.red,
+                    textColor: Colors.white,
+                    fontSize: 16.0
+                );
+                controller.reset();
+
+              },
+
+            ),
+
+
+
+
+
+
+
+
+
+            //////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+          ],
+        ),
+      ),
+
+
+
+
     );
   }
 }
--- lib/Screens/Government Fees/PayConfirmation.dart
+++ lib/Screens/Government Fees/PayConfirmation.dart
@@ -21,136 +21,138 @@ class _PayConfirmationPageState extends State<PayConfirmationPage> {
       ),
       backgroundColor: Color(0xFFFFF8F8),
 
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 40,),
-            Container(
-              height: 90,
-              width: 330,
-              child: ListTile(
-                leading: CircleAvatar(
-                    backgroundColor: Colors.white,
-                    child: Image.asset('assets/images/ait.png',height: 30,)),
-                title: Text('Land Development(LD) Tax'),
-                subtitle: Text('Govt. Fees'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 40,),
+              Container(
+                height: 90,
+                width: 330,
+                child: ListTile(
+                  leading: CircleAvatar(
+                      backgroundColor: Colors.white,
+                      child: Image.asset('assets/images/ait.png',height: 30,)),
+                  title: Text('Land Development(LD) Tax'),
+                  subtitle: Text('Govt. Fees'),
+                ),
               ),
-            ),
-            SizedBox(height: 10,),
+              SizedBox(height: 10,),
 
-            Container(
-              child: Table(
-                border: TableBorder.all(
-                    borderRadius: BorderRadius.circular(3.0),
-                    color: Colors.grey,
-                    style: BorderStyle.solid,
-                    width: 1),
+              Container(
+                child: Table(
+                  border: TableBorder.all(
+                      borderRadius: BorderRadius.circular(3.0),
+                      color: Colors.grey,
+                      style: BorderStyle.solid,
+                      width: 1),
 
-                children: [
-                  TableRow(
-                    decoration: BoxDecoration(
-                        color: Colors.white
+                  children: [
+                    TableRow(
+                      decoration: BoxDecoration(
+                          color: Colors.white
+                      ),
+                      children: [
+                        Column(children:[Text('Account Number \n 01771389547', style: GoogleFonts.openSans(fontSize: 16.0))]),
+                        Column(children:[Text('Account Name \nNafim Ahmed', style: GoogleFonts.openSans(fontSize: 16.0))]),
+                      ]
                     ),
-                    children: [
-                      Column(children:[Text('Account Number \n 01771389547', style: GoogleFonts.openSans(fontSize: 16.0))]),
-                      Column(children:[Text('Account Name \nNafim Ahmed', style: GoogleFonts.openSans(fontSize: 16.0))]),
-                    ]
-                  ),
 
-                ],
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 20,),
-            Container(
-              height: 150,
-              width: 330,
-              child: Table(
-                border: TableBorder.all(
-                    borderRadius: BorderRadius.circular(3.0),
-                    color: Colors.grey,
-                    style: BorderStyle.solid,
-                    width: 1),
+              SizedBox(height: 20,),
+              Container(
+                height: 150,
+                width: 330,
+                child: Table(
+                  border: TableBorder.all(
+                      borderRadius: BorderRadius.circular(3.0),
+                      color: Colors.grey,
+                      style: BorderStyle.solid,
+                      width: 1),
 
-                children: [
-                  TableRow(
-                      decoration: BoxDecoration(
-                          color: Colors.white
-                      ),
-                      children: [
-                    Column(children:[Text('Amount\n', style: GoogleFonts.openSans(fontSize: 16.0))]),
-                    Column(children:[Text('Charge\n', style: GoogleFonts.openSans(fontSize: 16.0))]),
-                    Column(children:[Text('Total\n', style: GoogleFonts.openSans(fontSize: 16.0))]),
-                  ]),
-                  TableRow(
-                      decoration: BoxDecoration(
-                          color: Colors.white
-                      ),
-                      children: [
-                    Column(children:[Text('৳ ', style: TextStyle(fontSize: 16.0))]),
-                    Column(children:[Text('+৳ 0.00', style: TextStyle(fontSize: 16.0))]),
-                    Column(children:[Text('৳ ', style: TextStyle(fontSize: 16.0))]),
-                  ]),
+                  children: [
+                    TableRow(
+                        decoration: BoxDecoration(
+                            color: Colors.white
+                        ),
+                        children: [
+                      Column(children:[Text('Amount\n', style: GoogleFonts.openSans(fontSize: 16.0))]),
+                      Column(children:[Text('Charge\n', style: GoogleFonts.openSans(fontSize: 16.0))]),
+                      Column(children:[Text('Total\n', style: GoogleFonts.openSans(fontSize: 16.0))]),
+                    ]),
+                    TableRow(
+                        decoration: BoxDecoration(
+                            color: Colors.white
+                        ),
+                        children: [
+                      Column(children:[Text('৳ ', style: TextStyle(fontSize: 16.0))]),
+                      Column(children:[Text('+৳ 0.00', style: TextStyle(fontSize: 16.0))]),
+                      Column(children:[Text('৳ ', style: TextStyle(fontSize: 16.0))]),
+                    ]),
 
-                ],
-              ),
-
-            ),
-
-            SizedBox(height: 20,),
-            // Text('   PIN',style: GoogleFonts.openSans(
-            //   fontSize: 16,
-            //     color: Colors.black,
-            //     fontWeight: FontWeight.w400
-            // ),),
-
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Container(
-                height: 40,width: 330,
-                decoration: BoxDecoration(
-                    border: Border.all(width: 1,color: Color(0xFFFCDEDE),),
-                    borderRadius: BorderRadius.circular(6.0),
-                    color: Colors.white
+                  ],
                 ),
 
-
-
-                child: TextField(
-                  //controller: _mobileNo,
-                  keyboardType: TextInputType.number,
-                  cursorColor: Colors.red.shade900,
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    prefixIcon: Icon(Icons.lock,size: 20,color: Colors.red.shade300,),
-                    hintText: 'Enter PIN',
-                    hintStyle: TextStyle(color: Colors.grey.shade400),
-                    suffixIcon: InkWell(
-                        onTap:() {
-                          //
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) {
-                                return ConfirmPaymentPage();
-                              },
-                            ),
-                          );
-
-                        },
-                        child: Icon(Icons.arrow_forward,size: 22,color: Colors.red.shade300,)),
-                    suffixIconColor: Color(0xFFFCDEDE),
-                  ),
-                ),
-
-
-
               ),
-            ),
+
+              SizedBox(height: 20,),
+              // Text('   PIN',style: GoogleFonts.openSans(
+              //   fontSize: 16,
+              //     color: Colors.black,
+              //     fontWeight: FontWeight.w400
+              // ),),
+
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  height: 40,width: 330,
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 1,color: Color(0xFFFCDEDE),),
+                      borderRadius: BorderRadius.circular(6.0),
+                      color: Colors.white
+                  ),
 
 
-          ],
+
+                  child: TextField(
+                    //controller: _mobileNo,
+                    keyboardType: TextInputType.number,
+                    cursorColor: Colors.red.shade900,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      prefixIcon: Icon(Icons.lock,size: 20,color: Colors.red.shade300,),
+                      hintText: 'Enter PIN',
+                      hintStyle: TextStyle(color: Colors.grey.shade400),
+                      suffixIcon: InkWell(
+                          onTap:() {
+                            //
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return ConfirmPaymentPage();
+                                },
+                              ),
+                            );
+
+                          },
+                          child: Icon(Icons.arrow_forward,size: 22,color: Colors.red.shade300,)),
+                      suffixIconColor: Color(0xFFFCDEDE),
+                    ),
+                  ),
+
+
+
+                ),
+              ),
+
+
+            ],
+          ),
         ),
       ),
     );
