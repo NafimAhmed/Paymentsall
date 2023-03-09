db95d00e4e6284cf01928573ebbcfb2af2d1176b
recen <recentnafimahmed@gmail.com>
2023-03-09 18:14:02 +0600

    nafim  Notification done  6:13pm 09/03/2023

--- lib/Screens/Notification/NotificationScreen.dart
+++ lib/Screens/Notification/NotificationScreen.dart
@@ -1,11 +1,17 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../../utils/app_layout.dart';
 
-class NotificationScreen extends StatelessWidget {
+class NotificationScreen extends StatefulWidget {
   const NotificationScreen({Key? key}) : super(key: key);
 
+  @override
+  State<NotificationScreen> createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,43 +33,46 @@ class NotificationScreen extends StatelessWidget {
                 itemCount: 10,
                 itemBuilder: (BuildContext context,int index){
 
-                  return Container(
+                  return InkWell(
+                    onTap: triggerNotification,
+                    child: Container(
 
-                    decoration: BoxDecoration(
-                        color: Colors.red.shade100,
-                        border: Border.all(
+                      decoration: BoxDecoration(
                           color: Colors.red.shade100,
-                        ),
-                        borderRadius: BorderRadius.all(Radius.circular(15))
-                    ),
-
-
-                    padding: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(10),vertical: AppLayout.getHeight(10)),
-                    margin: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(5),vertical: AppLayout.getHeight(10)),
-
-
-                   // margin: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(10),vertical: AppLayout.getHeight(20)),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-
-                        Text("Notification Title",
-                          style: GoogleFonts.openSans(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold
+                          border: Border.all(
+                            color: Colors.red.shade100,
                           ),
-                        ),
+                          borderRadius: BorderRadius.all(Radius.circular(15))
+                      ),
 
-                        Text("Notificaton Description",
 
+                      padding: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(10),vertical: AppLayout.getHeight(10)),
+                      margin: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(5),vertical: AppLayout.getHeight(10)),
+
+
+                     // margin: EdgeInsets.symmetric(horizontal: AppLayout.getWidth(10),vertical: AppLayout.getHeight(20)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+
+                          Text("Notification Title",
                             style: GoogleFonts.openSans(
-                            fontSize: 15,
-                            fontWeight: FontWeight.normal
-                        ),
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
 
-                        ),
+                          Text("Notificaton Description",
 
-                      ],
+                              style: GoogleFonts.openSans(
+                              fontSize: 15,
+                              fontWeight: FontWeight.normal
+                          ),
+
+                          ),
+
+                        ],
+                      ),
                     ),
                   );
 
@@ -77,5 +86,37 @@ class NotificationScreen extends StatelessWidget {
 
 
     );
+  }
+
+
+
+
+
+  triggerNotification()
+  {
+    AwesomeNotifications().createNotification(
+        content: NotificationContent(
+            id: 10,
+            channelKey: 'basic_channel',
+            title: 'Simple Notification',
+            body: 'this is notification body'
+        )
+    );
+  }
+
+
+
+
+  @override
+  void initState() {
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed){
+
+      if(!isAllowed)
+      {
+        AwesomeNotifications().requestPermissionToSendNotifications();
+      }
+
+    });
+    super.initState();
   }
 }
